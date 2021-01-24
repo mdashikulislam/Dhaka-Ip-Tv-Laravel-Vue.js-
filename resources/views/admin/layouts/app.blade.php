65cd2b9f082b1c189bc49e6832dd1982e3ac4702
mdashikulislam <ashik.nwu@gmail.com>
2021-01-25 01:37:04 +0600

    Login blade customize and admin authentication

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Dashboard</title>
+    <title>Dhaka IP TV</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -90,7 +90,13 @@
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    <a href="#" onclick="
+                                        event.preventDefault();
+                                        $('#logout-user').submit();
+                                    " class="btn btn-default btn-flat">Sign out</a>
+                                    <form action="{{route('logout')}}" id="logout-user" method="POST" style="display: none">
+                                        @csrf
+                                    </form>
                                 </div>
                             </li>
                         </ul>
