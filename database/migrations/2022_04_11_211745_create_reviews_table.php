eea873551f3135499e78ed451ed1c69e1824afa2
mdashikulislam <ashik.nwu@gmail.com>
2022-04-12 03:26:27 +0600

    review form add

---
+++ database/migrations/2022_04_11_211745_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('review');
+            $table->integer('rating')->default(0);
+            $table->enum('type',['Channel','Radio'])->default('Channel');
+            $table->enum('status',['Active','Inactive'])->default('Active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
