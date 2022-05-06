bf6ecaaa278f78b2e77da02900338ea681249212
mdashikulislam <ashik@gmail.com>
2022-05-06 12:01:31 +0600

    google tag insert

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -36,9 +36,23 @@
         gtag('js', new Date());
         gtag('config', 'G-T3QTC48DHS');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WV8XVVH');</script>
+    <!-- End Google Tag Manager -->
+
+
     @stack('css')
 </head>
 <body class="body">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV8XVVH"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @php
     $currentRoute = Route::currentRouteName();
 @endphp
